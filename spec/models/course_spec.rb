4eed26654db8953580014df97eed944a8e6bd3b7
username <wnt925@gmail.com>
2019-07-26 17:53:01 -0600

    Add model tests

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Course do
+  describe 'Relationships' do
+    it {should have_many :students}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+  end
+
+end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe Student do
+  describe 'Relationships' do
+    it {should have_many :courses}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :grade}
+  end
+
+end
